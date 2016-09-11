753a5f445676c5c2708ae837bc7596e02eb868ed
tony <908601756@qq.com>
2016-09-11 22:53:40 +0800

    更新商品详情

--- app/pages/ProductPage.js
+++ app/pages/ProductPage.js
@@ -25,9 +25,10 @@ import Loading from '../components/Loading';
 import Header from '../components/Header';
 import Common from '../common/constants';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Swiper from 'react-native-swiper';
 import {productView} from '../actions/productActions';
 
-export default class ProductPage extends Component {
+export default class ProductPage extends React.Component {
     componentDidMount() {
         //交互管理器在任意交互/动画完成之后，允许安排长期的运行工作. 在所有交互都完成之后安排一个函数来运行。
         InteractionManager.runAfterInteractions(() => {
@@ -39,25 +40,27 @@ export default class ProductPage extends Component {
     render() {
         const {productReducer} = this.props;
         let product = productReducer.product;
-        // alert(productReducer.isLoading);
-        // if (!productReducer.isLoading){
-        //     Alert.alert(product);
-        // }
 
         return (
-            <View style={styles.container}>
+            <View style={{flex: 1, backgroundColor: 'white'}}>
                 <Header
                     leftIcon='angle-left'
                     leftIconAction={()=>this.props.navigator.pop()}
                     title='商品详情'
                 />
-                {productReducer.isLoading ?
-                    <Loading /> :
-                    <View style={styles.content}>
-                        <Text>{product.name}</Text>
-                        <Text>{product.price}</Text>
-                    </View>
-                }
+                <View style={styles.mainWrap}>
+                    {productReducer.isLoading ?
+                        <Loading /> :
+                        <View
+                            bounces={false}
+                            showsVerticalScrollIndicator={false}
+                            style={styles.scrollView}
+                        >
+                            <Text>{product.name}</Text>
+                            <Text>{product.price}</Text>
+                        </View>
+                    }
+                </View>
                 <ToolBar style={{position: 'absolute', bottom: 0}}/>
             </View>
         )
@@ -67,7 +70,7 @@ export default class ProductPage extends Component {
 class ToolBar extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
+            <View style={styles.toolBarWrap}>
                 <TouchableOpacity style={styles.toolBarItem}>
                     <Icon
                         name="share-square-o"
@@ -91,29 +94,31 @@ class ToolBar extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection:'row',
-    },
-
-    content: {
+    //内容栏
+    mainWrap: {
         width: Common.window.width,
-        height: Common.window.height - 64 - 40,
+        height: Common.window.height - 64 - 20,
+    },
+    scrollView: {
+        height: Common.window.height - 64 - 20,
+        paddingTop: 25,
+        backgroundColor: 'rgb(241, 241, 241)',
     },
 
+    // 底部栏
+    toolBarWrap: {
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderTopColor: '#ccc',
+        borderTopWidth: 0.5,
+    },
     toolBarItem: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
     },
-
-    itemTitle: {
-        marginLeft: 5,
-        fontSize: 12,
-        color: 'gray'
-    },
-
     centerLine: {
         position: 'absolute',
         height: 20,
@@ -121,5 +126,10 @@ const styles = StyleSheet.create({
         top: 10,
         right: Common.window.width * 0.5,
         backgroundColor: '#ccc'
-    }
+    },
+    itemTitle: {
+        marginLeft: 5,
+        fontSize: 12,
+        color: 'gray'
+    },
 });
